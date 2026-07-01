d9c204033e4052d26e53d3b0bdbc93e514b6fb6e
Daniel Omowole <101469475+Sanmidaniels07@users.noreply.github.com>
2026-07-01 20:14:06 +0100

    email-service-fix

--- src/services/email.services.ts
+++ src/services/email.services.ts
@@ -1,30 +1,30 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
 
-export const transporter =
-  nodemailer.createTransport({
-    service: "gmail",
+const transportOptions: SMTPTransport.Options = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+};
 
-    auth: {
-      user:
-        process.env.EMAIL_USER,
+export const transporter = nodemailer.createTransport(transportOptions);
 
-      pass:
-        process.env.EMAIL_PASS,
-    },
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  html: string
+) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html,
   });
-
-  export const sendEmail =
-  async (
-    to: string,
-    subject: string,
-    html: string
-  ) => {
-    await transporter.sendMail({
-      from:
-        process.env.EMAIL_USER,
-
-      to,
-      subject,
-      html,
-    });
-  };
+};
